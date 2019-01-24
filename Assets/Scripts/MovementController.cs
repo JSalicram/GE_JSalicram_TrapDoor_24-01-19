e1c3201e9b29d1d4651f06a38a71a2281fee530a
JSalicram <JSalicram@users.github.com>
2019-01-24 12:28:14 +0000

    Initial commit

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    private Rigidbody2D theRigidBody;
+    public float maxHorixontalSpeed;
+    private float speedMultiplier;
+
+    void Start()
+    {
+        theRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        speedMultiplier = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        theRigidBody.velocity = new Vector2(maxHorixontalSpeed * speedMultiplier, theRigidBody.velocity.y);
+    }
+}
