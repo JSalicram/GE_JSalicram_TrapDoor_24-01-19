64bba12a61acef2238939c39fc14d681e416c58f
JSalicram <JSalicram@users.github.com>
2019-01-24 12:50:44 +0000

    Scripts Created

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void TriggerZone()
+    {
+        TrapDoorController.OpenTrap();
+    }
+}
